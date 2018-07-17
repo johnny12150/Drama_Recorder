00b070f6da7a7e2d0f9389b34726fd948a2970f8
johnny12150 <johnny12150@gmail.com>
2018-07-17 08:39:48 +0800

    login and logout functionality

---
+++ php/logout.php
@@ -0,0 +1,17 @@
+<?php
+   //將session清空
+   unset($_SESSION['username']);
+   echo '登出中......';
+   echo '<meta http-equiv=REFRESH CONTENT=1;url=login.php>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
